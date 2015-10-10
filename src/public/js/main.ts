83609e2a809949b8dbde78991d616592305674e4
progre <djyayutto@gmail.com>
2015-10-10 10:44:45 +0900

    Implement

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,17 +1,50 @@
 /// <reference path="typings.d.ts" />
 
 function main() {
+    $('#loading').hide();
     let params = parse(document.location.search);
-    location.href = `http://127.0.0.1:7144/pls/${params.get('id') }?tip=${params.get('tip') }`;
+    if (params['id'] == null || params['tip'] == null) {
+        Cookies.remove('ip');
+        Cookies.remove('port');
+        $('#clear').show();
+        return;
+    }
+    let ip = Cookies.get('ip');
+    let port = Cookies.get('port');
+    if (ip == null || port == null) {
+        firstTime(params);
+    } else {
+        everyTime(ip, port, params);
+    }
+}
+
+function firstTime(params: any) {
+    $('#firsttime').show();
+    $('#button').click(function() {
+        let ip = $('#ip').val();
+        let port = $('#port').val();
+        Cookies.set('ip', ip);
+        Cookies.set('port', port);
+        redirect(ip, port, params);
+    });
+}
+
+function everyTime(ip: string, port: string, params: any) {
+    $('#everytime').show();
+    redirect(ip, port, params);
+}
+
+function redirect(ip: string, port: string, params: any) {
+    location.href = `http://${ip}:${port}/pls/${params['id']}?tip=${params['tip']}`;
 }
 
 function parse(search: string) {
-    let params = new Map<string, string>();
+    let params: any = {};
     search.slice(1)
         .split('&')
         .map(x => x.split('='))
         .forEach(x => {
-            params.set(decodeURIComponent(x[0]), decodeURIComponent(x[1]));
+            params[decodeURIComponent(x[0])] = decodeURIComponent(x[1]);
         });
     return params;
 }
--- src/public/js/typings.d.ts
+++ src/public/js/typings.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="../../../node_modules/typescript/lib/lib.es6.d.ts" />
+/// <reference path="../../../typings/DefinitelyTyped/jquery/jquery.d.ts" />
+/// <reference path="../../../typings/DefinitelyTyped/js-cookie/js-cookie.d.ts" />
